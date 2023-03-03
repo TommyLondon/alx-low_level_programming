92479345577fae1e95afe17f3f0305834a1186dc
TommyLondon <johannkutumela@gmail.com>
2023-03-03 13:43:02 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,18 +1,20 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * rot13 - encode rot13
- * @s: pointer to string programs
- * return: *s
+ * rot13 - encoder rot13
+ * @s: pointer to string params
+ *
+ * Return: *s
  */
-char *rot13(char *);
+
+char *rot13(char *s)
 {
 	int i;
 	int j;
-	char data1[] =
-"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char datarot[] =
-"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
@@ -24,5 +26,5 @@ char *rot13(char *);
 			}
 		}
 	}
-return (s);
+	return (s);
 }
